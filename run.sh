955e11a45aa31b529599515e0286abf292e7dfd6
Christopher Rae <raecd123@gmail.com>
2022-12-08 14:13:10 +0000

    update to run

--- run.sh
+++ run.sh
@@ -6,7 +6,8 @@
 python3 main.py --experiment_type=H
 
 JAX_PLATFORM_NAME=gpu MPI4JAX_USE_CUDA_MPI=1 mpirun --np $NUM_GPUS --H $HOSTS --map-by slot --bind-to none -x NCCL_DEBUG=INFO -x LD_LIBRARY_PATH -x PATH --mca pml ob1 --mca btl ^openib --mca btl_tcp_if_include $MPI_NET_INT python3 main.py --experiment_type=C
-
+tc qdisc add dev $MPI_NET_INT root tbf rate 1gbit
 JAX_PLATFORM_NAME=gpu MPI4JAX_USE_CUDA_MPI=1 mpirun --np $NUM_GPUS --H $HOSTS --map-by slot --bind-to none -x NCCL_DEBUG=INFO -x LD_LIBRARY_PATH -x PATH --mca pml ob1 --mca btl ^openib --mca btl_tcp_if_include $MPI_NET_INT python3 main.py --experiment_type=E1
 JAX_PLATFORM_NAME=gpu MPI4JAX_USE_CUDA_MPI=1 mpirun --np $NUM_GPUS --H $HOSTS --map-by slot --bind-to none -x NCCL_DEBUG=INFO -x LD_LIBRARY_PATH -x PATH --mca pml ob1 --mca btl ^openib --mca btl_tcp_if_include $MPI_NET_INT python3 main.py --experiment_type=E2
 JAX_PLATFORM_NAME=gpu MPI4JAX_USE_CUDA_MPI=1 mpirun --np $NUM_GPUS --H $HOSTS --map-by slot --bind-to none -x NCCL_DEBUG=INFO -x LD_LIBRARY_PATH -x PATH --mca pml ob1 --mca btl ^openib --mca btl_tcp_if_include $MPI_NET_INT python3 main.py --experiment_type=E3
+tc qdisc del dev $MPI_NET_INT root
--- run_fast.sh
+++ run_fast.sh
@@ -1,5 +1,11 @@
+
+# $MPI_NET_INT is the interface name for the network that is used for MPI communication
 python3 main.py --experiment_type=H
+
+tc qdisc add dev $MPI_NET_INT root tbf rate 1gbit
 
 JAX_PLATFORM_NAME=gpu MPI4JAX_USE_CUDA_MPI=1 mpirun --np $NUM_GPUS --H $HOSTS --map-by slot --bind-to none -x NCCL_DEBUG=INFO -x LD_LIBRARY_PATH -x PATH --mca pml ob1 --mca btl ^openib --mca btl_tcp_if_include $MPI_NET_INT python3 main.py --experiment_type=C
 
 JAX_PLATFORM_NAME=gpu MPI4JAX_USE_CUDA_MPI=1 mpirun --np $NUM_GPUS --H $HOSTS --map-by slot --bind-to none -x NCCL_DEBUG=INFO -x LD_LIBRARY_PATH -x PATH --mca pml ob1 --mca btl ^openib --mca btl_tcp_if_include $MPI_NET_INT python3 main.py --experiment_type=E3
+
+tc qdisc del dev $MPI_NET_INT root
